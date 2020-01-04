69446f0055bc7099199206cfc16511f1d417078e
radhakrishna4687 <ioskrishna11@gmail.com>
2020-01-04 23:49:28 +0530

    main.tf terrafoms scritpts for aws

---
+++ main.tf
@@ -0,0 +1,97 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+#vpc
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags {
+        Name ="${var.vpc_name}"
+    }
+}
+#Internetgateway
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+    tags {
+        Name = "${var.IGW_Name}"
+    }
+  
+}
+#subnet1,subnet3,subnet3
+resource "aws_subnet" "subnet1-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet1_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "${var.public_subnet1_name}"
+    }  
+}
+
+#subnet2
+resource "aws_subnet" "subnet2-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet2_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "${var.public_subnet2_name}"
+    }  
+}
+#subnet3
+resource "aws_subnet" "subnet3-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet3_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "${var.public_subnet3_name}"
+    }  
+} 
+#route table
+resource "aws_route_table" "terraform-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+
+    tags {
+        Name = "${var.Main_Routeing_Table}"
+    }
+}    
+#routetable assocition
+resource "aws_route_table_association" "terraform-public" {
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    route_table_id = "${aws_route_table_association.terraform-public.id}"
+}
+#secutiy groups
+resource "aws_security_group" "allow_all" {
+    name = "allow_all"
+    description = "Allow all inbound traffic"
+    vpc_id = "${aws_vpc.default.id}"
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+#subnet_ids
+resource "aws_subnet_ids" "private" {
+    vpc_id = "${aws_vpc.default.id}"
+  
+}
+
+
